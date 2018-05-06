28ea93286952cd837cbc5b7773eda7e61e568cd0
Hunter Morrell <hunter.james.morrell@gmail.com>
2018-05-06 19:05:06 -0400

    modified install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/$olddir/.$file
+    mv ~/.$file $olddir/.$file
 done
 
 make
