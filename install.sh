a7620004b70a9be35c4ac4fe7bee9c583449427d
Hunter Morrell <hunter.james.morrell@gmail.com>
2019-10-01 08:04:49 -0400

    Updating install script to be better organized and actually do more things

--- install.sh
+++ install.sh
@@ -1,18 +1,124 @@
 #!/bin/bash
-############################
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# Taken from http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
-############################
+####################
+#
+# Variables
+#
+####################
 
-########## Variables
+repos_dir=~/repos/
+dotfiles_dir=~/repos/dotfiles
+packages="zsh polybar radare2"
 
-dir=~/repos/dotfiles                    # dotfiles directory
-olddir=~/repos/dotfiles_old             # old dotfiles backup directory
-files="aliases exports gitmodules xinitrc Xresources xserverrc zprofile zshrc"    # list of files/folders to symlink in homedir
+####################
+#
+# Installing required packages
+#
+####################
 
-##########
+echo "Installing required packages"
+platform=$(uname);
+# If the platform is Linux, try an apt-get to install zsh and then recurse
+if [[ $platform == 'Linux' ]]; then
+  if [[ -f /etc/redhat-release ]]; then
+    sudo yum install zsh
+  fi
+  if [[ -f /etc/debian_version ]]; then
+    sudo apt install zsh
+  fi
+fi
 
-# create dotfiles_old in homedir
+
+
+####################
+#
+# Creating directory structure
+#
+####################
+
+echo "Creating directory structure"
+mkdir ~/repos
+mkdir ~/AppImages
+mkdir ~/ISOs
+mkdir ~/security
+mkdir /virtual_machines
+
+
+####################
+#
+# Cloning and installing repos
+#
+####################
+
+echo "Cloning and installing repos"
+cd ~/repos
+
+echo "Nerd Fonts"
+git clone git@github.com:ryanoasis/nerd-fonts.git
+./nerd-fonts/install.sh
+
+echo "dotfiles"
+git clone --recurse-submodules git@github.com:HunterMorrell/dotfiles.git
+
+echo "light"
+git clone git@github.com:haikarainen/light.git
+
+echo "UserCSS-Styles"
+git clone git@github.com:HunterMorrell/UserCSS-Styles.git
+
+echo "ssh-ident"
+mkdir -p ~/bin; wget -O ~/bin/ssh goo.gl/MoJuKB; chmod 0755 ~/bin/ssh
+
+
+####################
+#
+# Configuring ZSH
+#
+####################
+
+echo "Configuring ZSH"
+
+
+
+###################
+#
+# Symlinking
+#
+####################
+
+echo "Symlinking"
+
+echo "Home"
+cd ~/repos/dotfiles/general
+ln -sf ~/repos/dotfiles/general/.aliases ~
+ln -sf ~/repos/dotfiles/general/.config ~
+ln -sf ~/repos/dotfiles/general/.exports ~
+ln -sf ~/repos/dotfiles/general/.xinitrc ~
+ln -sf ~/repos/dotfiles/general/.Xresources ~
+ln -sf ~/repos/dotfiles/general/.xserverrc ~
+
+echo "Vim"
+ln -sf ~/repos/dotfiles/vim ~
+mv ~/vim ~/.vim
+cd ~/repos/dotfiles/vim
+ln -sf ~/repos/dotfiles/vim/.vimrc ~
+ln -sf ~/repos/dotfiles/vim/colors ~/.local/share/nvim/site/
+ln -sf ~/repos/dotfiles/vim/pack ~/.local/share/nvim/site/
+
+echo "zsh"
+cd ~/repos/dotfiles/zsh
+ln -sf ~/repos/dotfiles/zsh/.zshrc ~
+ln -sf ~/repos/dotfiles/zsh/.zprofile ~
+
+
+####################
+#
+# Setting up machine-specific configurations
+#
+####################
+
+
+
+
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
 echo "...done"
