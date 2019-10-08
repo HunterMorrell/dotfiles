b9e576be93de57ba6dd22a890b14d1f877571a3d
Hunter Morrell <hunter.james.morrell@gmail.com>
2019-10-08 14:25:58 -0400

    Updating install script and package list

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 repos_dir=~/repos/
 dotfiles_dir=~/repos/dotfiles
-packages="zsh polybar radare2"
+packages="zsh polybar radare2 git alsa-utils build-essential cmake curl dpkg fwupd gcc gdb i3 i3lock light neovim python3 python3-pip ranger rofi ruby rxvt-unicode vim cairo libxcb python2 xcb-proto xcb-util-image xcb-util-wm xcb-util-xrm alsa-lib i3-wm wireless_tools"
 
 ####################
 #
@@ -20,10 +20,14 @@ platform=$(uname);
 # If the platform is Linux, try an apt-get to install zsh and then recurse
 if [[ $platform == 'Linux' ]]; then
   if [[ -f /etc/redhat-release ]]; then
-    sudo yum install zsh
+		for package in $packages; do
+			sudo dnf install $package
+		done
   fi
   if [[ -f /etc/debian_version ]]; then
-    sudo apt install zsh
+		for package in $packages; do
+			sudo apt -y install $package
+		done
   fi
 fi
 
@@ -68,6 +72,13 @@ git clone git@github.com:HunterMorrell/UserCSS-Styles.git
 echo "ssh-ident"
 mkdir -p ~/bin; wget -O ~/bin/ssh goo.gl/MoJuKB; chmod 0755 ~/bin/ssh
 
+echo "polybar"
+git clone --recursive https://github.com/polybar/polybar
+cd polybar
+sudo ./build.sh
+
+chmod +x ~/repos/dotfiles/general/colorls_install.sh && ~/repos/dotfiles/general/colorls_install.sh
+
 
 ####################
 #
@@ -77,6 +88,29 @@ mkdir -p ~/bin; wget -O ~/bin/ssh goo.gl/MoJuKB; chmod 0755 ~/bin/ssh
 
 echo "Configuring ZSH"
 
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        if [[ -f /etc/redhat-release ]]; then
+            sudo dnf -y install zsh
+        fi
+        if [[ -f /etc/debian_version ]]; then
+            sudo apt -y install zsh
+        fi
+    fi
+fi
 
 
 ###################
@@ -117,59 +151,3 @@ ln -sf ~/repos/dotfiles/zsh/.zprofile ~
 ####################
 
 
-
-
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir/.$file
-done
-
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
-        fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
-        fi
-    fi
-fi
-}
-
-install_zsh
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
-done
-
-chmod +x ./colorls_install.sh && ./colorls_install.sh
